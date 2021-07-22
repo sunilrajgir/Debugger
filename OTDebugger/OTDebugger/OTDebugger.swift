c744ba86054e35f6b35344eecbecd17980aaf16a
Sunil Kumar <sunil.kumar@telekom-digital.com>
2021-07-22 13:32:20 +0530

    added class

---
+++ OTDebugger/OTDebugger/OTDebugger.swift
@@ -0,0 +1,16 @@
+//
+//  OTDebugger.swift
+//  OTDebugger
+//
+//  Created by Sunil Kumar on 22/07/21.
+//
+
+import Foundation
+
+public class OTDebugger {
+    public let shared = OTDebugger()
+    private init(){}
+    func showDebugScreen() {
+        print("ppppp")
+    }
+}
