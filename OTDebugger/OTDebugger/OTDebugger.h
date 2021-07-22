fd57456888ad6c6058db79fc0e7dfcd5e797c41d
Sunil Kumar <sunil.kumar@telekom-digital.com>
2021-07-22 13:25:49 +0530

    added empty framework

---
+++ OTDebugger/OTDebugger/OTDebugger.h
@@ -0,0 +1,18 @@
+//
+//  OTDebugger.h
+//  OTDebugger
+//
+//  Created by Sunil Kumar on 22/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OTDebugger.
+FOUNDATION_EXPORT double OTDebuggerVersionNumber;
+
+//! Project version string for OTDebugger.
+FOUNDATION_EXPORT const unsigned char OTDebuggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OTDebugger/PublicHeader.h>
+
+
