2ec256eed4a971423db6717ed1cfb653b10bae4a
Sunil Kumar <sunil.kumar@telekom-digital.com>
2021-07-23 12:49:08 +0530

    added debug screen

---
+++ OTDebugger/OTDebugger/OTDebugManager.swift
@@ -0,0 +1,16 @@
+//
+//  OTDebugManager.swift
+//  OTDebugger
+//
+//  Created by Sunil Kumar on 23/07/21.
+//
+
+import Foundation
+
+public class OTDebugManager {
+    public init(){}
+
+    public func openDebugScreen() {
+        print("Welcome")
+    }
+}
